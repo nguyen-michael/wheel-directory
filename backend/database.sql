3de8350847b5825ce9d96e3abd1da59613008c75
unknown <nguyen.michl@gmail.com>
2020-04-02 22:52:45 -0500

    Finalize trigger functions and binds

--- backend/database.sql
+++ backend/database.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS rim (
   iso_diameter SMALLINT NOT NULL CONSTRAINT positive_iso_diameter_max_1000 CHECK(iso_diameter BETWEEN 0 AND 1000),
   erd REAL NOT NULL CONSTRAINT positive_erd CHECK(erd > 0),
   hole_count SMALLINT NOT NULL CONSTRAINT positive_hole_count_max_200 CHECK(hole_count BETWEEN 0 AND 200),
-  offset_spoke_bed REAL DEFAULT 0 CONSTRAINT positive_offset CHECK(offset_spoke_bed > 0),
+  offset_spoke_bed REAL DEFAULT 0 CONSTRAINT non_negative_offset CHECK(offset_spoke_bed >= 0),
   inner_width REAL,
   outer_width REAL,
   depth REAL,
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS hub (
   convertible BOOLEAN,
   driver VARCHAR CONSTRAINT driver_type CHECK(driver IN (NULL, 'XD', 'Hyperglide 10speed', 'Hyperglide 11speed', 'Hyperglide 7speed','XDR', 'Microspline', 'Uniglide', 'Helicomatic', 'Freewheel ISO', 'Freewheel M30', 'Fixed', 'Flipflop FW/FX', 'Flipflop FW/FW', 'Flipflop FX/FX', 'Proprietary/Other', 'Campagnolo 8speed', 'Campagnolo 9speed')),
   brake VARCHAR CONSTRAINT brake_type CHECK(brake IN (NULL, '6-Bolt', 'Centerlock', 'Drum', 'Roller', 'Coaster', 'Rohloff', '3-Bolt')),
-  boost BOOLEAN CONSTRAINT boost_type CHECK(boost IN (NULL, 'Boost', 'Superboost')),
+  boost VARCHAR CONSTRAINT boost_type CHECK(boost IN (NULL, 'Boost', 'Superboost')),
   web_url VARCHAR,
   notes VARCHAR,
   date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -90,11 +90,12 @@ CREATE FUNCTION check_wheel_holes()
 $check_wheel_holes$ LANGUAGE plpgsql;
 
 -- Functions to check if rim or hub is referenced in a wheel row. Prevent update of hole_count if so.
+-- if Hole count is being changed AND it's referenced in a wheel, raise exception otherwise carry on.
 CREATE FUNCTION prevent_hole_count_update_rim_if_referenced()
   RETURNS trigger AS $prevent_hole_count_update_rim_if_referenced$
     BEGIN
-      -- if Hole count is being changed AND it's referenced in a wheel, raise exception otherwise carry on.
-      IF (EXISTS (SELECT wheel.id FROM wheel WHERE OLD.id = wheel.rim_id) AND NEW.hole_count != OLD.hole_count) THEN
+     
+      IF EXISTS (SELECT wheel.id FROM wheel WHERE OLD.id = wheel.rim_id) AND NEW.hole_count != OLD.hole_count THEN
         RAISE EXCEPTION 'rim currently used in wheel, cannot change hole count.';
       END IF;
 
@@ -105,7 +106,6 @@ $prevent_hole_count_update_rim_if_referenced$ LANGUAGE plpgsql;
 CREATE FUNCTION prevent_hole_count_update_hub_if_referenced()
   RETURNS trigger AS $prevent_hole_count_update_hub_if_referenced$
     BEGIN
-      -- if Hole count is being changed AND it's referenced in a wheel, raise exception otherwise carry on.
       IF (EXISTS (SELECT wheel.id FROM wheel WHERE OLD.id = wheel.hub_id) AND NEW.hole_count != OLD.hole_count) THEN
         RAISE EXCEPTION 'hub currently used in wheel, cannot change hole count.';
       END IF;
