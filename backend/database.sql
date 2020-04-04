4962cd1839a2955655910f7ca650e24830bb67f9
unknown <nguyen.michl@gmail.com>
2020-04-03 21:57:12 -0500

    Modify database schema syntax

--- backend/database.sql
+++ backend/database.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS wheel (
   date_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT cross_between_0_and_5 CHECK(cross_pattern_nds BETWEEN 0 AND 5 AND cross_pattern_ds BETWEEN 0 AND 5),
   CONSTRAINT positive_lengths CHECK(spoke_length_nds > 0 AND spoke_length_ds > 0),
-  CONSTRAINT unique_wheel UNIQUE (rim_id, hub_id, cross_pattern_ds, cross_pattern_nds)
+  CONSTRAINT unique_wheel UNIQUE (rim_id, hub_id, cross_pattern_nds, cross_pattern_ds)
 );
 -- Rim
 -- Can add features like Weights later
@@ -24,10 +24,10 @@ CREATE TABLE IF NOT EXISTS rim (
   id SERIAL PRIMARY KEY,
   manufacturer VARCHAR NOT NULL,
   model_name VARCHAR NOT NULL,
-  iso_diameter SMALLINT NOT NULL CONSTRAINT positive_iso_diameter_max_1000 CHECK(iso_diameter BETWEEN 0 AND 1000),
-  erd REAL NOT NULL CONSTRAINT positive_erd CHECK(erd > 0),
-  hole_count SMALLINT NOT NULL CONSTRAINT positive_hole_count_max_200 CHECK(hole_count BETWEEN 0 AND 200),
-  offset_spoke_bed REAL DEFAULT 0 CONSTRAINT non_negative_offset CHECK(offset_spoke_bed >= 0),
+  iso_diameter SMALLINT NOT NULL,
+  erd REAL NOT NULL,
+  hole_count SMALLINT NOT NULL,
+  offset_spoke_bed REAL DEFAULT 0,
   inner_width REAL,
   outer_width REAL,
   depth REAL,
@@ -42,32 +42,42 @@ CREATE TABLE IF NOT EXISTS rim (
   notes VARCHAR,
   date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   date_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT unique_rim UNIQUE (manufacturer, model_name, iso_diameter, erd, hole_count, offset_spoke_bed, tubeless_compatible, rim_brake, material)
+  CONSTRAINT unique_rim UNIQUE (manufacturer, model_name, iso_diameter, erd, hole_count, offset_spoke_bed, tubeless_compatible, rim_brake, material),
+  CONSTRAINT positive_iso_diameter_max_1000 CHECK(iso_diameter BETWEEN 0 AND 1000),
+  CONSTRAINT positive_hole_count_max_200 CHECK(hole_count BETWEEN 0 AND 200),
+  CONSTRAINT non_negative_offset CHECK(offset_spoke_bed >= 0),
+  CONSTRAINT positive_erd CHECK(erd > 0)
 );
 -- Hub
 CREATE TABLE IF NOT EXISTS hub (
   id SERIAL PRIMARY KEY,
   manufacturer VARCHAR NOT NULL,
   model_name VARCHAR NOT NULL,
-  side VARCHAR NOT NULL CONSTRAINT front_or_rear CHECK(side IN ('front', 'rear')),
+  side VARCHAR NOT NULL,
   flange_pcd_nds REAL NOT NULL,
   flange_pcd_ds REAL NOT NULL,
   center_to_left REAL NOT NULL,
   center_to_right REAL NOT NULL,
   spacing REAL NOT NULL,
-  hole_count SMALLINT NOT NULL CONSTRAINT positive_hole_count_max_200 CHECK(hole_count BETWEEN 0 AND 200),
-  spoke_hole_diameter REAL NOT NULL,
+  hole_count SMALLINT NOT NULL,
+  spoke_hole_diameter REAL NOT NULL DEFAULT 2.6,
   convertible BOOLEAN,
-  driver VARCHAR CONSTRAINT driver_type CHECK(driver IN (NULL, 'XD', 'Hyperglide 10speed', 'Hyperglide 11speed', 'Hyperglide 7speed','XDR', 'Microspline', 'Uniglide', 'Helicomatic', 'Freewheel ISO', 'Freewheel M30', 'Fixed', 'Flipflop FW/FX', 'Flipflop FW/FW', 'Flipflop FX/FX', 'Proprietary/Other', 'Campagnolo 8speed', 'Campagnolo 9speed')),
-  brake VARCHAR CONSTRAINT brake_type CHECK(brake IN (NULL, '6-Bolt', 'Centerlock', 'Drum', 'Roller', 'Coaster', 'Rohloff', '3-Bolt')),
-  boost VARCHAR CONSTRAINT boost_type CHECK(boost IN (NULL, 'Boost', 'Superboost')),
+  driver VARCHAR,
+  brake VARCHAR,
+  boost VARCHAR,
+  weight REAL,
   web_url VARCHAR,
   notes VARCHAR,
   date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   date_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT positive_dimensions CHECK(spacing > 0 AND flange_pcd_nds > 0 AND flange_pcd_ds > 0 AND center_to_left > 0 AND center_to_right > 0 AND spoke_hole_diameter > 0),
   CONSTRAINT no_front_driver CHECK((side = 'front' AND driver IS NULL) OR (side = 'rear')),
-  CONSTRAINT unique_hub UNIQUE (manufacturer, model_name, side, spacing, hole_count, driver, brake, boost)
+  CONSTRAINT unique_hub UNIQUE (manufacturer, model_name, side, spacing, hole_count, driver, brake, boost),
+  CONSTRAINT front_or_rear CHECK(side IN ('front', 'rear')),
+  CONSTRAINT positive_hole_count_max_200 CHECK(hole_count BETWEEN 0 AND 200),
+  CONSTRAINT driver_type CHECK(driver IN ('XD', 'Hyperglide 10speed', 'Hyperglide 11speed', 'Hyperglide 7speed','XDR', 'Microspline', 'Uniglide', 'Helicomatic', 'Freewheel ISO', 'Freewheel M30', 'Fixed', 'Flipflop FW/FX', 'Flipflop FW/FW', 'Flipflop FX/FX', 'Proprietary/Other', 'Campagnolo 8speed', 'Campagnolo 9speed')),
+  CONSTRAINT brake_type CHECK(brake IN ('6-Bolt', 'Centerlock', 'Drum', 'Roller', 'Coaster', 'Rohloff', '3-Bolt')),
+  CONSTRAINT boost_type CHECK(boost IN ('Boost', 'Superboost'))
 );
 
 -- Function to prevent wheels from being made with hole mismatched rim and hubs
